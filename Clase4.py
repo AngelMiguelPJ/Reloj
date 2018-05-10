ed2b6409389d23728a9919f5dc4cf86924348489
1-21-01-MD-22\ESFOT <bryan.guerrero01@epn.edu.ec>
2018-05-10 09:11:46 -0500

    Reloj V1.1

--- Clase4.py
+++ Clase4.py
@@ -1,15 +1,24 @@
 import time
+import os
+
 
 print (time.localtime(),"\n")
 
+x = 1
 
-for x in range(1,61):
+
+while x != 0:
+  os.system('cls')
+
   t = time.localtime()
   hour = t[3]
   min = t[4]
   sec = t[5]
   print("La hora es ", hour,":" , min,":" , sec)
+
   time.sleep(1)
+  os.system('cls')
+
 
 
 
